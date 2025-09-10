33df4e5120f602e3ce97f1f85b4fcd77b1611141
itoshi-m-dev <emanuelkalison@gmail.com>
2025-09-10 16:13:19 -0300

    feature findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,11 +12,8 @@ public class Program {
 
         Department obj = new Department(1,"Books");
 
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
-
-
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
